ab4a4a927b8ea81a8b5cc4aab695b90b5e2dd027
Daniel Brandes <dbrandes@welfenlab.de>
2020-12-01 23:39:17 +0100

    package.json src/extension.ts
     - added params to use this extension with vs code dev container.
     - more flexibility for file-name regex (allow -,_,.)
     - svg should work.

--- src/extension.ts
+++ src/extension.ts
@@ -51,18 +51,46 @@ export function activate(context: vscode.ExtensionContext) {
 
     function updateDecorations() {
         if(!activeEditor) { return; }
-        const regEx = /<cmg "(([a-zA-Z0-9_\\\/]+)\.(png|jpg))">/g;
+        const regEx = /<cmg "(([a-zA-Z0-9-_\.\\\/]+)\.(png|jpg|svg))">/g;
         const text = activeEditor.document.getText();
         let cimgs: vscode.DecorationOptions[] = [];
         let match: RegExpExecArray;
         while(match = regEx.exec(text)) {
             const img: string = match[1];
-            const src = path.join(path.normalize(vscode.workspace.rootPath), img);
+            const resource = activeEditor.document.uri;
+            const commentimgConfig = vscode.workspace.getConfiguration('commentimg', resource);
+            const workspace_path_override = <string>commentimgConfig.get("workspace_path_override");
+            const home_dir_override = <string>commentimgConfig.get("home_dir_override");
+            const workspace_path_override_rel_to_home = <boolean>commentimgConfig.get("workspace_path_override_rel_to_home");
+            const workspace_path_override_dev_container = <boolean>commentimgConfig.get("workspace_path_override_dev_container");
+            let rootpath_extern: string | undefined;
+            rootpath_extern = vscode.workspace.rootPath;
+            if (workspace_path_override || workspace_path_override_rel_to_home) {
+                if (workspace_path_override_rel_to_home) {
+                    var homedir: string | undefined;
+                    homedir = require('os').homedir();
+                    if (home_dir_override && workspace_path_override_dev_container) {
+                        homedir = home_dir_override;
+                    }
+                    rootpath_extern = homedir;
+                } else {
+                    rootpath_extern = ""
+                }
+                rootpath_extern = path.join(rootpath_extern,workspace_path_override);
+            }
+            var rootpath_intern: string | undefined;
+            rootpath_intern = vscode.workspace.rootPath;
+            if (!workspace_path_override_dev_container){
+                rootpath_intern = rootpath_extern;
+            }
+            const src_intern = path.join(path.normalize(vscode.workspace.rootPath), img);
+            const src_extern = path.join(path.normalize(rootpath_extern), img);
 
-            if(fs.existsSync(src)) {
+            if(fs.existsSync(src_intern)) {
                 let s = activeEditor.document.positionAt(match.index);
                 let e = activeEditor.document.positionAt(match.index + match[0].length);
-                let decoration = { range: new vscode.Range(s, e), hoverMessage: `![](file:///${src})` };
+
+                let decoration = { range: new vscode.Range(s, e), hoverMessage: `![](file:///${src_extern})` };
                 cimgs.push(decoration);
             }
         }
